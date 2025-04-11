2ebf03cc5261d04d3fddeb515785ee3d0bcda06e
MohammedKamle <mohammed.kamle@yahoo.com>
2025-04-11 16:01:51 +0530

    first commit

---
+++ src/test/java/com/lambdatest/TestNGTodo3.java
@@ -0,0 +1,89 @@
+package com.lambdatest;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGTodo3 {
+
+    private RemoteWebDriver driver;
+    private String Status = "failed";
+
+    @BeforeMethod
+    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+        String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+        ;
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platform", "MacOS Catalina");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("version", "latest");
+        caps.setCapability("build", "TestNG With Java");
+        caps.setCapability("name", m.getName() + this.getClass().getName());
+        caps.setCapability("plugin", "git-testng");
+        caps.setCapability("performance",true);
+        caps.setCapability("network", true);
+        caps.setCapability("console", true);
+        
+        caps.setCapability("commandLog", true);
+        caps.setCapability("systemLog", true);
+        caps.setCapability("terminal", true);
+        caps.setCapability("video", true);
+         caps.setCapability("accessibility", true);
+        
+        caps.setCapability("accessibility.bestPractice", true);
+        caps.setCapability("accessibility.needsReview", true);
+
+        String[] Tags = new String[] { "Feature", "Tag", "Moderate" };
+        caps.setCapability("tags", Tags);
+
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
+    }
+
+    @Test
+    public void basicTest() throws InterruptedException {
+        String spanText;
+        System.out.println("Loading Url");
+        Thread.sleep(100);
+        driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+        System.out.println("Checking Box");
+        driver.findElement(By.name("li1")).click();
+
+        System.out.println("Checking Another Box");
+        driver.findElement(By.name("li2")).click();
+
+        System.out.println("Checking Box");
+        driver.findElement(By.name("li3")).click();
+
+   
+
+        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
+        driver.findElement(By.id("addbutton")).click();
+
+       
+        Status = "passed";
+        Thread.sleep(800);
+
+        System.out.println("TestFinished");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.executeScript("lambda-status=" + Status);
+        driver.quit();
+    }
+
+}
